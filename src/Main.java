8653991fd8a37fa5252d8dce526927bcc6b08e16
2523350 <ememejia2106@gmail.com>
2025-11-07 13:25:28 -0500

    Added methods in class main

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,10 @@ public class Main {
     public static void main(String[] args) {
 
     }
+    void showMember(){}
+    void showMovie(){}
+    void addMember(){}
+    void addMovie(){}
+    void rentMovie(){}
+    void payment(){}
 }
